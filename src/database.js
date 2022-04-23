0cfd4c605727f74b7db4be75f7bda60f9c769f17
Geru <gianestevezruiz@hotmail.com>
2022-04-23 18:47:46 -0500

    a

--- src/database.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://mongo/mydatabase')
-    .then(db => console.log('Db is conneced to', db.connection.host))
-    .catch(err => console.error(err));
